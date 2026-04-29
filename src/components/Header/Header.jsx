b0d360ce0e785df14bb8819a1a69d3995be7ea9a
Nathaniel <nathanielmujesia@gmail.com>
2026-04-29 22:04:28 +0300

    add header layout and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,9 @@
+import { Link } from "react-router-dom";
+
 import ThemeSwitcher from "../theme-switcher/ThemeSwitcher";
+
+
+import "./header.css";
 
 import logo from "../../assets/logo/logo.svg";
 
@@ -8,11 +13,13 @@ function Header() {
     return (
         <header className="primary-header">
             <div className="container">
-                <div className="header-content-wrapper| flex">
-                    {/* logo */}
-                    <div className="logo-wrapper">
-                        <img src={logo} alt="Logo" className="header-logo" />
-                    </div>
+                <div className="header-content-wrapper | flex">
+                    <Link to="/">
+                        {/* logo */}
+                        <div className="logo-wrapper">
+                            <img src={logo} alt="Logo" className="header-logo" />
+                        </div>
+                    </Link>
                     {/* theme switcher */}
                     <ThemeSwitcher />
                 </div>
